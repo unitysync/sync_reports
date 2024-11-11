34c504bc3358be66e3ed0ae9def7fbbe7bc5d25e
sync <68277325+unitysync@users.noreply.github.com>
2024-11-12 03:57:45 +1000

    ci(workflow): fix workflow js module error

--- .github/actions/bump-manifest.js
+++ .github/actions/bump-manifest.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
+import { readFileSync, writeFileSync } from "fs";
 const version = process.env.TGT_RELEASE_VERSION;
 const newVersion = version.replace("v", "");
-const manifestFile = fs.readFileSync("fxmanifest.lua", { encoding: "utf8" });
+const manifestFile = readFileSync("src/fxmanifest.lua", { encoding: "utf8" });
 const newFileContent = manifestFile.replace(
   /\bversion\s+(.*)$/gm,
   `version '${newVersion}'`
 );
 
-fs.writeFileSync("fxmanifest.lua", newFileContent);
+writeFileSync("src/fxmanifest.lua", newFileContent);
